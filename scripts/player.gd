51c970aff7ccf11781ca7db45d3b22273b9b45ef
Marcelo-Guaraglia <marcelo.guaraglia@globant.com>
2025-02-17 15:21:28 -0300

    Attack functionality
    Hay un ataque, no hace daño

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,17 @@
 extends Node2D
 
+
+
+var directions = {
+	"UP": Vector2.UP, 
+	"DOWN": Vector2.DOWN, 
+	"LEFT": Vector2.LEFT,
+	"RIGHT": Vector2.RIGHT  
+	}
+@onready var attack_sprite: Sprite2D = $CharacterBody2D/AttackSprite
+
+var lastDir = "DOWN"
+
 # Define the size of the grid
 @export var GRID_SIZE = 64
 @export var move_duration = 0.1  # Duration in seconds to complete the move
@@ -13,23 +25,36 @@ var move_timer: float = 0.0
 func _ready():
 	# Reference the CharacterBody2D node
 	character_body = $CharacterBody2D
+	attack_sprite.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("attack"):
+		basicAttack(lastDir)
+		print("ataque!")
 	if not moving:
 		# Check for input and set the direction
 		if Input.is_action_just_pressed("ui_up"):
 			target_velocity = Vector2.UP * (GRID_SIZE / move_duration)
 			moving = true
+			lastDir = "UP"
+			print(lastDir)
 		elif Input.is_action_just_pressed("ui_down"):
 			target_velocity = Vector2.DOWN * (GRID_SIZE / move_duration)
 			moving = true
+			lastDir = "DOWN"
+			print(lastDir)
 		elif Input.is_action_just_pressed("ui_left"):
 			target_velocity = Vector2.LEFT * (GRID_SIZE / move_duration)
 			moving = true
+			lastDir = "LEFT"
+			print(lastDir)
 		elif Input.is_action_just_pressed("ui_right"):
 			target_velocity = Vector2.RIGHT * (GRID_SIZE / move_duration)
+			print(target_velocity)
 			moving = true
+			lastDir = "RIGHT"
+			print(lastDir)
 
 	if moving:
 		# Calculate the movement step
@@ -50,3 +75,18 @@ func _process(delta):
 func _physics_process(_delta):
 	if not moving:
 		character_body.position = character_body.position.snapped(Vector2(GRID_SIZE, GRID_SIZE))
+
+func basicAttack(dir):
+# Set the position of the attack sprite
+	var direction_vector = directions[dir]
+	attack_sprite.position = direction_vector * GRID_SIZE  # Ajusta la posición según la dirección
+
+	# Show the attack sprite
+	attack_sprite.visible = true
+	# Optional: Add a timer to hide the attack sprite after a short delay
+	var attack_timer = get_tree().create_timer(0.2) # Oculta el sprite después de 0.2 segundos
+	attack_timer.timeout.connect(_hide_attack_sprite)
+
+# Function to hide the attack sprite
+func _hide_attack_sprite():
+	attack_sprite.visible = false
