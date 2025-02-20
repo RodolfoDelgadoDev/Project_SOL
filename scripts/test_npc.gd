0c983614f8ab5e353978c48576aeaeec42925114
Marcelo-Guaraglia <marcelo.guaraglia@globant.com>
2025-02-20 15:17:40 -0300

    UI Dialogo
    Updateado cosas de diálogo

--- scripts/test_npc.gd
+++ scripts/test_npc.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 var dialNum : int = 0
+@export var npc_portrait: Texture  # Ensure this is declared as Texture
 
 func _on_area_2d_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
 	if area.is_in_group("weapon"):
 		# Get the parent node and check if allBottles is true
 		var parent = get_parent()
 		if dialNum == 0:
-			parent.start_typewriter("¡Qué ganas de comer un alfajor...!")
+			parent.start_typewriter("¡Qué ganas de comer un alfajor...!", npc_portrait)
 			dialNum += 1
 		else:
-			parent.start_typewriter("¡La salida está a tu izquierda!")
+			parent.start_typewriter("¡La salida está a tu izquierda!", npc_portrait)
