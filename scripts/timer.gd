b799b21a50dfe4675c7030c54499493d16b4e575
Marcelo-Guaraglia <marcelo.guaraglia@globant.com>
2025-02-25 17:27:33 -0300

    UI
    Agregada UI de santi
    Tiene bottle counter

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -1,14 +1,25 @@
 extends Node2D
 
 var segundos = GameManager.Segundos
-@onready var label: Label = $Label
+@export var hp_full : Texture
+@export var hp_80 : Texture
+@export var hp_60 : Texture
+@export var hp_40 : Texture
+@export var hp_20 : Texture
+@export var hp_dead : Texture
+
+@onready var barra: TextureProgressBar = $TextureProgressBar
 @onready var timer: Timer = $Timer
+@onready var box_Sprite : Sprite2D = $Sprite2D
+@onready var botellaCounter : RichTextLabel = $RichTextLabel
 
 func _ready() -> void:
-	updateText()
+	var SceneManager = get_parent().get_parent()
+	barra.max_value = SceneManager.segundos
+	updateBar()
 	
 func _process(_delta: float) -> void:
-	updateText()
+	updateBar()
 	if GameManager.Segundos <= 0: ##This kills the player
 		await wait_seconds(3)
 		var parent = get_parent()
@@ -19,8 +30,26 @@ func _on_timer_timeout() -> void:
 	if (GameManager.Segundos > 0):
 		GameManager.Segundos -=1
 
-func updateText() -> void:
-	label.text = str(GameManager.Segundos)
+func updateBar() -> void:
+	var SceneManager = get_parent().get_parent()
+	botellaCounter.text = ("X " + str(SceneManager.bottleNum))
+	if SceneManager.allBottles == true:
+		botellaCounter.push_color(Color("yellow"))
+		botellaCounter.text = ("X " + str(SceneManager.bottleNum))
+	barra.value = GameManager.Segundos
+	if GameManager.Segundos >= 80 :
+		box_Sprite.texture = hp_full
+	if GameManager.Segundos < 80:
+		box_Sprite.texture = hp_80
+		if GameManager.Segundos < 60:
+			box_Sprite.texture = hp_60
+			if GameManager.Segundos < 40:
+				box_Sprite.texture = hp_40
+				if GameManager.Segundos < 20:
+					box_Sprite.texture = hp_20
+					if GameManager.Segundos == 0:
+						box_Sprite.texture = hp_dead
+			
 	
 func wait_seconds(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
