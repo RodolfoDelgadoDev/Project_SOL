efdb32f9e7b48af379837779ac339526e7f14f8f
Merzbro00 <142639878+Merzbro00@users.noreply.github.com>
2025-03-09 01:31:26 -0300

    Nuevo nivel chaser
    Te persige el perro fantasma

--- scripts/Traps/trap1.gd
+++ scripts/Traps/trap1.gd
@@ -1,26 +1,26 @@
 extends Node2D
 
-var reached_goal : bool = false
-var scene_change_triggered : bool = false  # New flag to track if scene change has been initiated
-@export var npc_portrait: Texture  # Ensure this is declared as Texture
+@export var damage: int
+@export var attackSFX: AudioStream
+@export var destroySFX: AudioStream
+@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
+@onready var sprite: AnimatedSprite2D = $Area2D/AnimatedSprite2D
+@onready var collision: CollisionShape2D = $Area2D/CollisionShape2D
 
-func _process(_delta: float) -> void:
-	if reached_goal and not scene_change_triggered:  # Check if scene change has not been triggered yet
-		var SceneManager = get_parent()
-		if SceneManager.is_typewriter_active == false:
-			scene_change_triggered = true  # Set the flag to true to indicate scene change is being triggered
-			SceneManager.change_scene("res://Scenes/Levels/descanso.tscn")
-	
 func _on_area_2d_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
-	if area.is_in_group("player"):
-		var SceneManager = get_parent()
-		SceneManager.stop_timer()
-		if SceneManager.descanso == true:
-			pass #mandar a otra escena
-		else:
-			# Get the parent node and check if allBottles is true
-			if SceneManager.has_method("get") and SceneManager.get("allBottles"):
-				SceneManager.start_typewriter("Encontraste todas las botellas!", npc_portrait)
-			else:
-				SceneManager.start_typewriter("Te faltaron algunas botellas!", npc_portrait)
-		reached_goal = true
+		if area.is_in_group("weapon"):
+			audio_player.stream = destroySFX
+			audio_player.play()  # Play attack sound
+			collision.disabled = true
+			sprite.visible = false
+			#await audio_player.finished
+			queue_free()
+			
+		if area.is_in_group("player"):
+			audio_player.stream = attackSFX
+			audio_player.play()  # Play attack sound
+			GameManager.takeDamage(damage)
+			collision.disabled = true
+			sprite.visible = false
+			#await audio_player.finished
+			queue_free()
