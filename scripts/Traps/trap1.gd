1d79e5a5fa334e95cfa62b37e41c39af7f429a9f
Merzbro00 <142639878+Merzbro00@users.noreply.github.com>
2025-03-09 12:47:29 -0300

    Ghost type enemy
    Ahora el fantasma te persigue

--- scripts/Traps/trap1.gd
+++ scripts/Traps/trap1.gd
@@ -5,22 +5,26 @@ extends Node2D
 @export var destroySFX: AudioStream
 @onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 @onready var sprite: AnimatedSprite2D = $Area2D/AnimatedSprite2D
-@onready var collision: CollisionShape2D = $Area2D/CollisionShape2D
+
+var activated : bool = false
+
 
 func _on_area_2d_area_shape_entered(_area_rid: RID, area: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
 		if area.is_in_group("weapon"):
-			audio_player.stream = destroySFX
-			audio_player.play()  # Play attack sound
-			collision.disabled = true
-			sprite.visible = false
-			#await audio_player.finished
-			queue_free()
+			if activated == false:
+				activated = true
+				audio_player.stream = destroySFX
+				audio_player.play()  # Play attack sound
+				sprite.visible = false
+				await audio_player.finished
+				queue_free()
 			
 		if area.is_in_group("player"):
-			audio_player.stream = attackSFX
-			audio_player.play()  # Play attack sound
-			GameManager.takeDamage(damage)
-			collision.disabled = true
-			sprite.visible = false
-			#await audio_player.finished
-			queue_free()
+			if activated == false:
+				activated = true
+				audio_player.stream = attackSFX
+				audio_player.play()  # Play attack sound
+				sprite.visible = false
+				GameManager.takeDamage(damage)
+				await audio_player.finished
+				queue_free()
