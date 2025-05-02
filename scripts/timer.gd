4158e00468d549805e8e6826bbb998c51d7fac5f
Merzbro00 <142639878+Merzbro00@users.noreply.github.com>
2025-05-02 00:14:54 -0300

    musica y shaders y fixes
    Probando que todos los niveles funquen
    Agregada musica a cada nivel
    Agregado sfx
    Agregados shaders en timer

--- scripts/timer.gd
+++ scripts/timer.gd
@@ -12,13 +12,25 @@ var segundos = GameManager.Segundos
 @onready var timer: Timer = $Timer
 @onready var box_Sprite: Sprite2D = $Sprite2D
 @onready var botellaCounter: RichTextLabel = $RichTextLabel
+@onready var botella: Sprite2D = $botella
 
 var is_scene_change_in_progress: bool = false  # State variable to track scene change
+var Max_Segundos
+var current_bottles = 0
+
+#variables para shaders
+var wave_amplitude : float = 1
+var aberration : float = 0
+var glitch : float = 0
+
+var box_speed = 0
+var box_aberration = 0
 
 func _ready() -> void:
 	var SceneManager = get_parent().get_parent()
 	barra.max_value = SceneManager.segundos
 	box_Sprite.texture = hp_full
+	Max_Segundos = segundos
 	updateBar()
 
 func _process(_delta: float) -> void:
@@ -37,24 +49,58 @@ func updateBar() -> void:
 	var SceneManager = get_parent().get_parent()
 	botellaCounter.text = ("X " + str(SceneManager.bottleTotal - SceneManager.bottleNum))
 	if SceneManager.allBottles == true:
-		botellaCounter.push_color(Color("yellow"))
 		botellaCounter.text = ("X " + str(SceneManager.bottleTotal - SceneManager.bottleNum) + "!")
+	if Max_Segundos < GameManager.Segundos:
+		Max_Segundos = GameManager.Segundos
+		barra.max_value = GameManager.Segundos
 	barra.value = GameManager.Segundos
+	if current_bottles < SceneManager.bottleNum:
+		current_bottles = SceneManager.bottleNum
+		wave_amplitude += 1
+		aberration += 0.1
+		glitch += 0.1
+		botella.material.set_shader_parameter("wave_amplitude", wave_amplitude)
+		botella.material.set_shader_parameter("aberration_speed", aberration)
+		botella.material.set_shader_parameter("glitch_intensity", glitch)
 
 	var porcentaje = (GameManager.Segundos / float(barra.max_value)) * 100
 
 	if porcentaje >= 80:
 		box_Sprite.texture = hp_full
+		box_speed = 0
+		box_aberration = 0
+		box_Sprite.material.set_shader_parameter("wave_speed", box_speed)
+		box_Sprite.material.set_shader_parameter("aberration_speed", box_aberration)
 	elif porcentaje >= 60:
 		box_Sprite.texture = hp_80
+		box_speed = 0.83
+		box_aberration = 0.33
+		box_Sprite.material.set_shader_parameter("wave_speed", box_speed)
+		box_Sprite.material.set_shader_parameter("aberration_speed", box_aberration)
 	elif porcentaje >= 40:
 		box_Sprite.texture = hp_60
+		box_speed = 0.83*2
+		box_aberration = 0.33*2
+		box_Sprite.material.set_shader_parameter("wave_speed", box_speed)
+		box_Sprite.material.set_shader_parameter("aberration_speed", box_aberration)
 	elif porcentaje >= 20:
 		box_Sprite.texture = hp_40
+		box_speed = 0.83*3
+		box_aberration = 0.33*3
+		box_Sprite.material.set_shader_parameter("wave_speed", box_speed)
+		box_Sprite.material.set_shader_parameter("aberration_speed", box_aberration)
 	elif porcentaje > 0:
 		box_Sprite.texture = hp_20
+		box_speed = 0.83*4
+		box_aberration = 0.33*4
+		box_Sprite.material.set_shader_parameter("wave_speed", box_speed)
+		box_Sprite.material.set_shader_parameter("aberration_speed", box_aberration)
 	else:
 		box_Sprite.texture = hp_dead
+		box_speed = 0.83*5
+		box_aberration = 0.33*5
+		box_Sprite.material.set_shader_parameter("wave_speed", box_speed)
+		box_Sprite.material.set_shader_parameter("aberration_speed", box_aberration)
 
 func wait_seconds(seconds: float) -> void:
 	await get_tree().create_timer(seconds).timeout
